13d8c91670ddc7a52872f428b818496c402765f4
Axel Grard <phidoraxgame@gmail.com>
2022-06-16 11:56:48 +0200

    Ajout try catch dans UserController

--- back-end/L3Projet/L3Projet.WebAPI/Controllers/UserController.cs
+++ back-end/L3Projet/L3Projet.WebAPI/Controllers/UserController.cs
@@ -2,25 +2,39 @@
 using L3Projet.Common.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace L3Projet.WebAPI.Controllers {
-	[ApiController]
-	[Route("[controller]")]
-	public class UserController : ControllerBase {
-		private readonly IUsersService usersService;
+namespace L3Projet.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUsersService usersService;
 
-		public UserController(IUsersService usersService) {
-			this.usersService = usersService;
-		}
+        public UserController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
 
-		[HttpGet("/all")]
-		public ActionResult<IEnumerable<User>> GetAll() {
-			var users = usersService.GetAllUsers();
+        [HttpGet("/all")]
+        public ActionResult<IEnumerable<User>> GetAll()
+        {
+            try
+            {
+                var users = usersService.GetAllUsers();
 
-			if (users.Any()) {
-				return Ok(users);
-			}
+                if (users.Any())
+                {
+                    return Ok(users);
+                }
 
-			return NoContent();
-		}
-	}
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+    }
 }
+
