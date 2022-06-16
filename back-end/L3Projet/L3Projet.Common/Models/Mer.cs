c4e89ddabf30030dd97b6190018e675e3ea6a2c2
Scherier <dareka@outlook.fr>
2022-06-16 14:13:08 +0200

    Test models => migration 1

---
+++ back-end/L3Projet/L3Projet.Common/Models/Mer.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace L3Projet.Common.Models {
+	public class Mer {
+		public Guid ID_Mer { get; set; }
+		public float Nombre_d_ile { get; set; }
+	}
+}
